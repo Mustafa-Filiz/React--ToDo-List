ae41da38c8926b23221962b8ed0257db8fe961b1
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-02 01:23:15 +0300

    tabs comp modified

--- src/components/TabsComp.js
+++ src/components/TabsComp.js
@@ -1,75 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { makeStyles } from '@mui/styles';
+import Tasks from './Tasks';
+
+const useStyles = makeStyles({
+    tabField: {
+        width: 200,
+    },
+});
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
 }
 
 function TabsComp() {
-  const [value, setValue] = useState(0);
+    const [value, setValue] = useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 100 }}
-    >
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
-      >
-        <Tab label="Today" {...a11yProps(0)} />
-        <Tab label="This Week" {...a11yProps(1)} />
+    const classes = useStyles();
 
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        First Item
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                bgcolor: 'background.paper',
+                display: 'flex',
+                height: 200,
+            }}
+        >
+            <Tabs
+                className={classes.tabField}
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                sx={{ borderRight: 1, borderColor: 'divider' }}
+            >
+                <Tab label="Tasks" {...a11yProps(0)} />
+                <Tab label="This Week" {...a11yProps(1)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <Tasks />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+        </Box>
+    );
 }
 
 export default TabsComp;
