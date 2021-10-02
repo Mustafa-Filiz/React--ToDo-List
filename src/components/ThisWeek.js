497882e8a8341c6bed29ffb645f631df833b25f7
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-03 02:15:06 +0300

    updates

--- src/components/ThisWeek.js
+++ src/components/ThisWeek.js
@@ -1,56 +1,27 @@
-import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import { Grid } from '@mui/material';
+import React, { useContext } from 'react';
+import { TodoListContext } from '../contexts/TodoListContext';
+import DayCardComp from './DayCardComp';
 
 function ThisWeek() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    const { todoList } = useContext(TodoListContext);
+    const days = Object.keys(todoList);
+    const tasks = Object.values(todoList);
+    let task;
+
+
+    return (
+        <Grid container spacing={2} justifyContent="center" alignItems="center" sx={{margin : 0, padding : 0, maxWidth : 1300}}>
+            {days.map((day) => {
+              tasks.map((item) => {return task=item})
+              return (
+                <Grid item>
+                  <DayCardComp day={day} task={task} />
+                </Grid>
+              )
+            })}
+        </Grid>
+    );
 }
 
 export default ThisWeek;
