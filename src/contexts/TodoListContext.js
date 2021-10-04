5a38769fbec4c74367e512f97b89177aae3f596b
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-04 14:06:38 +0300

    updates

--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -2,22 +2,43 @@ import React, { useState, createContext } from 'react';
 
 export const TodoListContext = createContext();
 
-const initialState = {
-    Mustafa: ['merhaba dünya ', 'world'],
-    Tuesday: ['hello', 'world'],
-    Wednesday: ['hello', 'world'],
-    Thursday: ['hello', 'world'],
-    Friday: ['hello', 'world'],
-    Saturday: ['hello', 'world'],
-    Sunday: ['hello', 'world'],
-};
+const initialState = [
+    { id: 1, task: 'ptesi', day: 'Monday' },
+    { id: 2, task: 'salı', day: 'Tuesday' },
+    { id: 3, task: 'çarş', day: 'Wednesday' },
+    { id: 4, task: 'perş', day: 'Thursday' },
+    { id: 5, task: 'cum', day: 'Friday' },
+    { id: 6, task: 'cts', day: 'Saturday' },
+    { id: 7, task: 'paz', day: 'Sunday' },
+];
 
 const TodoListContextProvider = ({ children }) => {
-
     const [todoList, setTodoList] = useState(initialState);
+    const [days, setDays] = useState([
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday',
+    ]);
+    const [newTask, setNewTask] = useState('');
+    const [taskDay, setTaskDay] = useState('');
+
+    // handleTaskAdd = () => {};
+
+    const values = {
+        todoList,
+        days,
+        newTask,
+        setNewTask,
+        taskDay,
+        setTaskDay,
+    };
 
     return (
-        <TodoListContext.Provider value={{ todoList }}>
+        <TodoListContext.Provider value={values}>
             {children}
         </TodoListContext.Provider>
     );
