0c7053d6ca1a1ae8b6419b291de37fdcc78541ce
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-04 23:43:32 +0300

    last commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,55 +1,40 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-// import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-// import FormGroup from '@mui/material/FormGroup';
 import {
     DarkModeRounded,
     EventAvailableRounded,
     LightModeRounded,
 } from '@mui/icons-material';
-import { Checkbox } from '@mui/material';
-import { ColorModeContext, ThemeContext } from '../contexts/ThemeContext';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { ColorModeContext } from '../contexts/ThemeContext';
 import IconButton from '@mui/material/IconButton';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-
+import { useTheme } from '@mui/material/styles';
 
 function NavBar() {
-    const [auth, setAuth] = useState(true);
     const theme = useTheme();
-    const colorMode = React.useContext(ColorModeContext);
+    const colorMode = useContext(ColorModeContext);
 
     return (
-        <div style={{ color: theme.text, bgcolor: theme.background }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <EventAvailableRounded fontSize="large" />
-                    <Typography
-                        variant="h5"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        Do Something...
-                    </Typography>
-                    <IconButton
-                        sx={{ ml: 1 }}
-                        onClick={colorMode.toggleColorMode}
-                        color="inherit"
-                    >
-                        {theme.palette.mode === 'dark' ? (
-                            <Brightness7Icon />
-                        ) : (
-                            <Brightness4Icon />
-                        )}
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <AppBar position="static">
+            <Toolbar>
+                <EventAvailableRounded fontSize="large" />
+                <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                    Do Something...
+                </Typography>
+                <IconButton
+                    sx={{ ml: 1 }}
+                    onClick={colorMode.toggleColorMode}
+                    color="inherit"
+                >
+                    {theme.palette.mode === 'dark' ? (
+                        <LightModeRounded fontSize="large" />
+                    ) : (
+                        <DarkModeRounded fontSize="large" />
+                    )}
+                </IconButton>
+            </Toolbar>
+        </AppBar>
     );
 }
 
