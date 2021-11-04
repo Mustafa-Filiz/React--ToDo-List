3ef0903d705bfc0e8848467e4a3b2d3d27155ccd
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-04 10:00:35 +0300

    unnecessary elements deletd

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
