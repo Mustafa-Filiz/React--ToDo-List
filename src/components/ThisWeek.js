e62c11be3a4ac33c876db70b579d890695e52e55
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-28 01:25:12 +0300

    updates

--- src/components/ThisWeek.js
+++ src/components/ThisWeek.js
@@ -1,11 +1,23 @@
 import React, { useContext, useState } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import { TodoListContext } from '../contexts/TodoListContext';
 import DayCardComp from './DayCardComp';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => {
+    return {
+        paper: {
+            padding: '2rem',
+        },
+    };
+});
 
 function ThisWeek() {
+    const classes = useStyles();
     const { todoList, days } = useContext(TodoListContext);
     const [dailyTasks, setDailyTasks] = useState([]);
+
+    // console.log(todoList);
 
     return (
         <Grid
@@ -15,7 +27,17 @@ function ThisWeek() {
             alignItems="center"
             sx={{ margin: 0, padding: 0, maxWidth: 1300 }}
         >
-            Hello
+            {days.map((day, index) => {
+                const items = todoList.filter((todo) => todo.day === day);
+                return (
+                    <Paper className={classes.paper} key={index}>
+                        {day}
+                        {items.map((item) => (
+                            <li key={item.id}> {item.task}</li>
+                        ))}
+                    </Paper>
+                );
+            })}
         </Grid>
     );
 }
