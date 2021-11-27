81016ddecc39e3e576a630c0bef547cade7e3dbd
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-28 01:24:40 +0300

    updates

--- src/components/DayCardComp.js
+++ src/components/DayCardComp.js
@@ -1,13 +1,11 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { ArrowRightRounded } from '@mui/icons-material';
-// import { todoListContext } from '../contexts/TodoListContext';
 
 function DayCardComp({ day, dailyTasks }) {
-    // const { todoList } = useContext(todoListContext);
 
     return (
         <Card sx={{ minWidth: 220 }}>
