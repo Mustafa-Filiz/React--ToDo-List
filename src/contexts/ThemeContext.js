e5c34866f35441f141a9669ddb1cb0c43ec98fc1
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-28 01:25:17 +0300

    updates

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 
@@ -29,7 +29,6 @@ export default function ToggleColorMode({children}) {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
           {children}
-        {/* <MyApp /> */}
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
